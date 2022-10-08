1a73e46788d5b8adf2096f983ed8e2ee1161dbdd
archebaldo <zakharov90@gmail.com>
2022-10-08 16:56:19 +0300

    Update component InputItem (add events and types props)

--- src/components/input-item/input-item.ts
+++ src/components/input-item/input-item.ts
@@ -3,15 +3,30 @@ import Component from 'core/component';
 import './input-item.pcss';
 
 type InputItemProps = {
+  onFocus?: () => void;
+  onBlur?: () => void;
   type: string;
   name: string;
   placeholder: string;
-  className: string;
+  className?: string;
 };
 
-export class InputItem extends Component {
+type ComponentProps = InputItemProps & {
+  events: {
+    focus?: (evt: FocusEvent) => void;
+    blur?: (evt: FocusEvent) => void;
+  };
+};
+
+export class InputItem extends Component<ComponentProps> {
   constructor(props: InputItemProps) {
-    super(props);
+    super({
+      ...props,
+      events: {
+        focus: props.onFocus,
+        blur: props.onBlur,
+      },
+    });
   }
 
   protected render() {
