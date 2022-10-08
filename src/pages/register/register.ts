99b8120db5e162391e8abbb2e21c9018d4b0b40f
archebaldo <zakharov90@gmail.com>
2022-10-08 17:01:38 +0300

    Update page - Register (add validate on blur/focus)

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,56 +1,106 @@
 import Component from 'core/component';
 
+import { validateForm } from 'helpers/validate/validate-form';
+
+import { ValidateType } from 'helpers/validate/const';
+
 import './register.pcss';
 
 export class Register extends Component {
-  render() {
+  constructor() {
+    super();
+
+    this.setProps({
+      onFocus: this.onFocus,
+      onBlur: this.onBlur,
+    });
+  }
+
+  private onFocus = (evt: FocusEvent): void => this.validate(evt);
+  private onBlur = (evt: FocusEvent): void => this.validate(evt);
+
+  private validate = (evt: FocusEvent): void => {
+    const target = evt.target as HTMLInputElement;
+    const currentRefName = target.name;
+    const errorRef = this.refs[currentRefName].refs.errorRef;
+
+    const text = validateForm({
+      type: target.name as ValidateType,
+      value: target.value,
+    });
+
+    errorRef.setProps({ text });
+  };
+
+  protected render() {
     return `
       <main class="register-page">
         <div class="register-page__container">
           <section class="register">
             <p class="register__text">Регистрация</p>
             <form class="register__form">
-                {{{ InputItem
+                {{{ InputItemControlled
                     type="email"
                     name="email"
                     placeholder="Почта"
                     className="input-item--register register__input-item"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="email"
                 }}}
-                {{{ InputItem
+                {{{ InputItemControlled
                     type="text"
                     name="login"
                     placeholder="Логин"
                     className="input-item--register register__input-item"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="login"
                 }}}
-                {{{ InputItem
+                {{{ InputItemControlled
                     type="text"
-                    name="first_name"
+                    name="firstName"
                     placeholder="Имя"
                     className="input-item--register register__input-item"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="firstName"
                 }}}
-                {{{ InputItem
+                {{{ InputItemControlled
                     type="text"
-                    name="second_name"
+                    name="secondName"
                     placeholder="Фамилия"
                     className="input-item--register register__input-item"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="secondName"
                 }}}
-                {{{ InputItem
+                {{{ InputItemControlled
                     type="tel"
                     name="phone"
                     placeholder="Телефон"
                     className="input-item--register register__input-item"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="phone"
                 }}}
-                {{{ InputItem
+                {{{ InputItemControlled
                     type="password"
                     name="password"
                     placeholder="Пароль"
                     className="input-item--register register__input-item"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="password"
                 }}}
-                {{{ InputItem
+                {{{ InputItemControlled
                     type="password"
-                    name="password-repeat"
+                    name="passwordRepeat"
                     placeholder="Пароль еще раз"
                     className="input-item--register register__input-item"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="passwordRepeat"
                 }}}
                 <button type="button" class="register__button">Зарегистрироваться</button>
             </form>
