897581b5319ad4a984ac8200fbcbb09cc4a5b5ac
archebaldo <zakharov90@gmail.com>
2022-10-08 18:55:42 +0300

    Update types for routes

--- src/core/router.ts
+++ src/core/router.ts
@@ -1,10 +1,12 @@
+import Component from 'core/component';
+
 import { Login } from 'pages/login/login';
 import { Register } from 'pages/register/register';
 import { Chats } from 'pages/chats/chats';
 import { Profile } from 'pages/profile/profile';
 import { ChangePassword } from 'pages/change-password/change-password';
 
-type Routes = Record<string, any>;
+type Routes = Record<string, () => Component>;
 
 export const routes: Routes = {
   '/': () => new Login(),
