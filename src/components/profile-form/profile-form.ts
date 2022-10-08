85b7bf6ce5cb1b78f59172ad0c30bd5ce7444fe2
archebaldo <zakharov90@gmail.com>
2022-10-08 16:57:31 +0300

    Update component ProfileForm (add validate on blur/focus)

--- src/components/profile-form/profile-form.ts
+++ src/components/profile-form/profile-form.ts
@@ -1,8 +1,37 @@
 import Component from 'core/component';
 
+import { validateForm } from 'helpers/validate/validate-form';
+
+import { ValidateType } from 'helpers/validate/const';
+
 import './profile-form.pcss';
 
 export class ProfileForm extends Component {
+  constructor() {
+    super();
+
+    this.setProps({
+      onFocus: this.onFocus,
+      onBlur: this.onBlur,
+    });
+  }
+
+  private onFocus = (evt: FocusEvent): void => this.validate(evt);
+  private onBlur = (evt: FocusEvent): void => this.validate(evt);
+
+  private validate = (evt: FocusEvent): void => {
+    const target = evt.target as HTMLInputElement;
+    const currentRefName = target.name;
+    const errorRef = this.refs[currentRefName].refs.errorRef;
+
+    const text = validateForm({
+      type: target.name as ValidateType,
+      value: target.value,
+    });
+
+    errorRef.setProps({ text });
+  };
+
   protected render() {
     return `
       <section class="profile">
@@ -10,66 +39,84 @@ export class ProfileForm extends Component {
           <div class="profile-avatar">
             <img src="https://www.fillmurray.com/130/130" alt="avatar">
             <p class="profile-avatar__name">Вадим</p>
-            {{{ InputItem
-                type="file"
-                name="avatar"
-                placeholder=""
-                className="input-item--avatar"
+            {{{ InputItemControlled
+                  type="file"
+                  name="avatar"
+                  placeholder=""
+                  className="input-item--avatar"
             }}}
           </div>
-          <fieldset class="profile-fieldset" disabled>
+          <fieldset class="profile-fieldset">
             <div class="profile-field">
               <span class="profile-field__title">Почта</span>
-              {{{ InputItem
-                  type="email"
-                  name="email"
-                  placeholder="pochta@yandex.ru"
-                  className="input-item--profile-email profile-field__input"
+              {{{ InputItemControlled
+                    type="email"
+                    name="email"
+                    placeholder="pochta@yandex.ru"
+                    className="input-item--profile-email profile-field__input"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="email"
               }}}
             </div>
             <div class="profile-field">
               <span class="profile-field__title">Логин</span>
-              {{{ InputItem
-                  type="text"
-                  name="login"
-                  placeholder="ivanivanov"
-                  className="input-item--profile-login profile-field__input"
+              {{{ InputItemControlled
+                    type="text"
+                    name="login"
+                    placeholder="ivanivanov"
+                    className="input-item--profile-login profile-field__input"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="login"
               }}}
             </div>
             <div class="profile-field">
               <span class="profile-field__title">Имя</span>
-              {{{ InputItem
-                  type="text"
-                  name="first_name"
-                  placeholder="Иван"
-                  className="input-item--profile-name profile-field__input"
+              {{{ InputItemControlled
+                    type="text"
+                    name="firstName"
+                    placeholder="Иван"
+                    className="input-item--profile-name profile-field__input"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="firstName"
               }}}
             </div>
             <div class="profile-field">
               <span class="profile-field__title">Фамилия</span>
-              {{{ InputItem
-                  type="text"
-                  name="second_name"
-                  placeholder="Иванов"
-                  className="input-item--profile-surname profile-field__input"
+              {{{ InputItemControlled
+                    type="text"
+                    name="secondName"
+                    placeholder="Иванов"
+                    className="input-item--profile-surname profile-field__input"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="secondName"
               }}}
             </div>
             <div class="profile-field">
               <span class="profile-field__title">Имя в чате</span>
-              {{{ InputItem
-                  type="text"
-                  name="display_name"
-                  placeholder="Ivan"
-                  className="input-item--profile-nickname profile-field__input"
+              {{{ InputItemControlled
+                    type="text"
+                    name="displayName"
+                    placeholder="Ivan"
+                    className="input-item--profile-nickname profile-field__input"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="displayName"
               }}}
             </div>
             <div class="profile-field">
               <span class="profile-field__title">Телефон</span>
-              {{{ InputItem
-                  type="tel"
-                  name="phone"
-                  placeholder="+7 (909) 967 30 30"
-                  className="input-item--profile-phone profile-field__input"
+              {{{ InputItemControlled
+                    type="tel"
+                    name="phone"
+                    placeholder="+7 (909) 967 30 30"
+                    className="input-item--profile-phone profile-field__input"
+                    onFocus=onFocus
+                    onBlur=onBlur
+                    ref="phone"
               }}}
             </div>
           </fieldset>
