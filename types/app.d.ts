0c6b439e85cb62e8c7a9d920a149046c507ecd3c
archebaldo <zakharov90@gmail.com>
2022-12-09 17:55:49 +0300

    Update global types

--- types/app.d.ts
+++ types/app.d.ts
@@ -1,8 +1,11 @@
 declare global {
   export type Nullable<T> = T | null;
-
   export type Keys<T extends Record<string, unknown>> = keyof T;
   export type Values<T extends Record<string, unknown>> = T[Keys<T>];
+  export type Indexed<T = unknown> = {
+    [key in string]: T;
+  };
+  export type AnyProps = Record<string, unknown>;
 }
 
-export {}
+export {};
