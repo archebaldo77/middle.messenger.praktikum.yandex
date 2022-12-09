6dc750b8d08c2c1f22f63226e3310a0c589e1456
archebaldo <zakharov90@gmail.com>
2022-12-09 21:40:16 +0300

    Add allComponents and allPages

---
+++ src/components/all-components/all-components.ts
@@ -0,0 +1,43 @@
+import InputItem from 'components/input-item/input-item';
+import InputItemControlled from 'components/input-item-controlled/input-item-controlled';
+import ErrorMessage from 'components/error-message/error-message';
+import ChatAside from 'components/chat-aside/chat-aside';
+import Search from 'components/search/search';
+import ChatList from 'components/chat-list/chat-list';
+import ChatItem from 'components/chat-item/chat-item';
+import Messages from 'components/messages/messages';
+import User from 'components/user/user';
+import MessagesList from 'components/messages-list/messages-list';
+import MessagesItem from 'components/messages-item/messages-item';
+import MessagesControls from 'components/messages-controls/messages-controls';
+import MessagesClip from 'components/messages-clip/messages-clip';
+import MessagesSend from 'components/messages-send/messages-send';
+import MessagesEmpty from 'components/messages-empty/messages-empty';
+import ProfileForm from 'components/profile-form/profile-form';
+import ProfileControls from 'components/profile-controls/profile-controls';
+import ChangePasswordForm from 'components/change-password-form/change-password-form';
+import LinkBack from 'components/link-back/link-back';
+import Button from 'components/button/button';
+
+export const allComponents = [
+  InputItem,
+  InputItemControlled,
+  ErrorMessage,
+  ChatAside,
+  Search,
+  ChatList,
+  ChatItem,
+  Messages,
+  User,
+  MessagesList,
+  MessagesItem,
+  MessagesControls,
+  MessagesClip,
+  MessagesSend,
+  MessagesEmpty,
+  ProfileForm,
+  ProfileControls,
+  ChangePasswordForm,
+  LinkBack,
+  Button,
+];
