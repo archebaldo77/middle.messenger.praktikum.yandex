4b3f669b4980662da35f2a16665cd5357c0d4d5a
archebaldo <zakharov90@gmail.com>
2022-12-09 17:26:33 +0300

    Update ChangePassword page (add router, and types to props)

--- src/pages/change-password/change-password.ts
+++ src/pages/change-password/change-password.ts
@@ -2,19 +2,38 @@ import Component from 'core/component';
 
 import { ComponentName } from 'helpers/const';
 
+import { withRouter } from 'HOCs/with-router';
+
+import type { Router } from 'router/router';
+
 import './change-password.pcss';
 
-export class ChangePassword extends Component {
+type ChangePasswordProps = {
+  router: Router;
+  onClickBack: () => void;
+};
+
+export class ChangePassword extends Component<ChangePasswordProps> {
   static componentName = ComponentName.ChangePassword;
+
+  constructor(props: ChangePasswordProps) {
+    super(props);
+
+    this.setProps({
+      onClickBack: () => this.props.router.go(`/profile`),
+    });
+  }
 
   protected render() {
     return `
       <main class="change-password-page">
         <div class="change-password-page__container">
           {{{ ChangePasswordForm }}}
-          {{{ LinkBack }}}
+          {{{ LinkBack onClick=onClickBack }}}
         </div>
      </main>
     `;
   }
 }
+
+export default withRouter(ChangePassword);
