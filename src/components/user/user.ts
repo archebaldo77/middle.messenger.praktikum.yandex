74e82cda49f516eae4f994252d5a8cc8b17ee730
archebaldo <zakharov90@gmail.com>
2022-12-09 22:20:55 +0300

    Update User component

--- src/components/user/user.ts
+++ src/components/user/user.ts
@@ -2,36 +2,106 @@ import Component from 'core/component';
 
 import { ComponentName } from 'helpers/const';
 
+import { withStore } from 'HOCs/with-store';
+
+import { addUserToChat } from 'actions/add-user-to-chat';
+import { deleteUserFromChat } from 'actions/delete-user-from-chat';
+
+import type { initialState } from 'store/initial-state';
+import type { Store } from 'store/store';
+
 import './user.pcss';
 
-export class User extends Component {
+type UserProps = {
+  onAddUser: () => void;
+  onDeleteUser: () => void;
+  chat: Chat;
+  users: string[];
+  selectedChat: string;
+  userId: string;
+  store: Store;
+};
+
+export class User extends Component<UserProps> {
   static componentName = ComponentName.User;
+
+  constructor(props: UserProps) {
+    super(props);
+
+    this.setProps({
+      onAddUser: this.onAddUser,
+      onDeleteUser: this.onDeleteUser,
+    });
+  }
+
+  protected onAddUser = () => {
+    const id = prompt(`Введите ID пользователя`);
+
+    if (id === null || id === ` `) {
+      return;
+    }
+
+    if (String(this.props.userId) === id) {
+      alert(`Вы уже в чате`);
+
+      return;
+    }
+
+    this.props.store.dispatch(addUserToChat(id, this.props.selectedChat));
+  };
+
+  protected onDeleteUser = () => {
+    const id = prompt(`Введите ID пользователя`);
+
+    if (id === null || id === ` `) {
+      return;
+    }
+
+    if (String(this.props.userId) === id) {
+      alert(`Нельзя удалить создателя чата`);
+
+      return;
+    }
+
+    this.props.store.dispatch(deleteUserFromChat(id, this.props.selectedChat));
+  };
 
   protected render() {
     return `
       <div class="user">
-        <section class="user-profile">
+        <div class="user-profile__wrapper">
+          <section class="user-profile">
           <div class="user-profile__avatar">
-          <img src="https://www.fillmurray.com/47/47" alt="avatar" />
+          ${
+            this.props.chat.avatar
+              ? `<img src=https://ya-praktikum.tech/api/v2/resources{{src}} alt="avatar" width=60 height=60 />`
+              : `<div class="chat-item__mock-avatar">Изображение отсутствует</div>`
+          }
           </div>
-          <p class="user-profile__name">Вадим</p>
-        </section>
+          <p class="user-profile__name">${this.props.chat.title}</p>
+          </section>
+          <p class="user-profile__login">Пользователи: ${this.props.users.join(
+            `, `
+          )}</p>
+        </div>
         <div class="user__menu">
-          <div class="user__dots">
-          <svg
-            width="3"
-            height="16"
-            viewBox="0 0 3 16"
-            fill="#1E1E1E"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="1.5" cy="2" r="1.5" fill="inherit"></circle>
-            <circle cx="1.5" cy="8" r="1.5" fill="inherit"></circle>
-            <circle cx="1.5" cy="14" r="1.5" fill="inherit"></circle>
-          </svg>
-          </div>
+          {{{ Button text="Добавить пользователя" type="button" className="user__button user__button-success" onClick=onAddUser }}}
+          {{{ Button text="Удалить пользователя" type="button" className="user__button user__button-danger" onClick=onDeleteUser }}}
         </div>
       </div>
     `;
   }
 }
+
+const mapStateToProps = (state: typeof initialState) => {
+  return {
+    chat: state.chat.list.find(
+      ({ id }) => Number(id) === Number(state.chat.selectedChat)
+    ),
+    users: state.chat.usersInSelectedChat,
+    selectedChat: state.chat.selectedChat,
+    userId: state.user.data.id,
+  };
+};
+
+export default withStore(User, mapStateToProps);
