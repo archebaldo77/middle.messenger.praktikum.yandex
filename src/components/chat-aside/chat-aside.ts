573eac45207a9bef6316a644ca7179335d1066b3
archebaldo <zakharov90@gmail.com>
2022-12-09 17:49:22 +0300

    Update ChatAside component (change export to default)

--- src/components/chat-aside/chat-aside.ts
+++ src/components/chat-aside/chat-aside.ts
@@ -4,7 +4,7 @@ import { ComponentName } from 'helpers/const';
 
 import './chat-aside.pcss';
 
-export class ChatAside extends Component {
+export default class ChatAside extends Component<AnyProps> {
   static componentName = ComponentName.ChatAside;
 
   protected render() {
